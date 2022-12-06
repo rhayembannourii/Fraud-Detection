93387818ecaf0f539c9d1c9689379a3ec81d7aa2
rhayemkernel <rhayembannouri1@gmail.com>
2022-12-06 15:40:36 +0200

    Fraud Detection App

--- backend/main.py
+++ backend/main.py
@@ -66,4 +66,4 @@ def inference(data: DataValidation):
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:backend", host='127.0.0.1', port=8000, reload=True)
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
